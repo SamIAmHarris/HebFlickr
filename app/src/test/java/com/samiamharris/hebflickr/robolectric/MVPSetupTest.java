1dd501f21c98c3db9320ac1de3d4714bffc8b71f
Sam Harris <SamIAm@Sams-MacBook-Pro-2.local>
2018-03-04 22:53:19 -0600

    Add extra roboelectric test to test that presenter detaches view

--- app/src/test/java/com/samiamharris/hebflickr/robolectric/MVPSetupTest.java
+++ app/src/test/java/com/samiamharris/hebflickr/robolectric/MVPSetupTest.java
@@ -1,13 +1,16 @@
 package com.samiamharris.hebflickr.robolectric;
 
 import com.samiamharris.hebflickr.feature.home.HomeActivity;
+import com.samiamharris.hebflickr.feature.home.HomeContract;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by SamIAm on 3/2/18.
@@ -23,4 +26,20 @@ public class MVPSetupTest {
         assertNotNull(activity.getPresenter().getRepo());
         assertNotNull(activity.getPresenter().getView());
     }
+
+    @Test
+    public void testDetachViewOnDestroy() {
+        ActivityController controller = Robolectric.buildActivity(HomeActivity.class).create().start();
+        HomeActivity activity = (HomeActivity) controller.get();
+
+        HomeContract.Presenter presenter = activity.getPresenter();
+
+        assertNotNull(presenter.getView());
+
+        controller.destroy();
+
+        assertNull(presenter.getView());
+    }
+
+
 }
